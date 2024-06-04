2422810fbdeef109d027d9a983b4b693e07c25d9
MatthewProg1 <matvey061207@gmail.com>
2024-06-04 21:03:28 +0300

    Add files via upload

---
+++ NetworkItemsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NetworkItemsManager : NetworkBehaviour
+{
+    public List<InventoryItemObject> inventoryItems;
+
+    public void SpawnItem(string itemName)
+    {
+        if (!IsServer) 
+            return;
+
+        var item = Instantiate(inventoryItems.Where(x => x.ItemName == itemName).First());
+        item.GetComponent<NetworkObject>().Spawn();
+    }
+}
